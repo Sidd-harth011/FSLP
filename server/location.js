b47711043356369786979316664e2d7064ffbfc5
sidd-harth011 <siddharthgautam573@gmail.com>
2025-08-08 14:49:27 +0530

    changes

--- server/location.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-fetch(`https://ipinfo.io/json?token=` + process.env.IPINFO_TOKEN)
-  .then(res => res.json())
-  .then(data => {
-    const loc = data.loc.split(',');
-    console.log('Approximate Latitude:', loc[0]);
-    console.log('Approximate Longitude:', loc[1]);
-  });
